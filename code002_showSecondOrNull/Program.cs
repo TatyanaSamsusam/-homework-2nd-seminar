0f1bf70ae61af7159a69dec7dfdf79b4b1df92af
TatyanaSamsusam <99976556+TatyanaSamsusam@users.noreply.github.com>
2022-03-14 21:01:16 +0300

    couldn't push, dragged manually

---
+++ code002_showSecondOrNull/Program.cs
@@ -0,0 +1,18 @@
+﻿int number = new int();
+System.Console.WriteLine("\ninput a number: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+int result = number / 10;
+
+if (result < 10)
+{
+    Console.WriteLine($"there is no third digit");
+}
+
+if (result > 10)
+{
+    string str = number.ToString();
+    Console.WriteLine($"third digit is " + str[2]);
+}
+
+

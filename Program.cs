a304cc17a57634868017eb4861d041b73401a405
Tatyana <android87@list.ru>
2022-03-14 17:38:20 +0300

    show second didgit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿int number = new int();
+System.Console.WriteLine("\ninput a number from 100 till 999: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+number = number / 10;
+int result = number % 10;
+
+Console.WriteLine($"second didgit is {result}");

f0e74ac5c4bdec196e66079ae48ccda3febd3134
TatyanaSamsusam <99976556+TatyanaSamsusam@users.noreply.github.com>
2022-03-16 14:34:28 +0300

    code003_weekdays_dayoff

---
+++ code003_weekdays/Program.cs
@@ -0,0 +1,44 @@
+﻿int Days = new int ();
+
+Console.WriteLine ("type a number of weekday and i write if it's day off");
+Days = Convert.ToInt32(Console.ReadLine());
+
+switch (Days)
+{
+    case 1:
+    {
+        Console.WriteLine("No");
+        break;
+    }
+    case 2:
+    {
+        Console.WriteLine("No");
+        break;
+    }
+        case 3:
+    {
+        Console.WriteLine("No");
+        break;
+    }
+        case 4:
+    {
+        Console.WriteLine("No");
+        break;
+    }    
+        case 5:
+    {
+        Console.WriteLine("No");
+        break;
+    }
+        case 6:
+    {
+        Console.WriteLine("YES!");
+        break;
+    }
+        case 7:
+    {
+        Console.WriteLine("still yes");
+        break;
+    }
+}
+
